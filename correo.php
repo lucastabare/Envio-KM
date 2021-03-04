122d162c5f073d0bd416f8c406ca0883c03ff8af
Lucas Tabare <76732872+lucastabare@users.noreply.github.com>
2021-03-03 22:19:56 -0300

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,12 @@
+﻿<?php
+   	
+ $destino = "teleworldcordoba@gmail.com";
+    $patente = $_POST["patente"];
+    $kilometraje = $_POST["kilometraje"];
+    $usuariowfx = $_POST["usuariowfx"];
+    $contenido = "Patente: " . $patente . "\nKilometraje: " . $kilometraje . "\nUsuario WFX: " . $usuariowfx;
+    @mail($destino, $patente, $contenido);
+   	if (@mail) {
+   		echo "<h1>KILOMETRAJE ENVIADO EXITOSAMENTE</h1>";
+   	}
+ ?>
